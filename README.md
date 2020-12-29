63fefa48a462e36a5f32bded3d737ca62409a639
abdalrahman-alhmouz <abood.alhmouz121@gmail.com>
2020-12-29 14:46:10 +0200

    init main

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # readme-dd
+our project 
+We did a project which is our cv work based on asking the browser several personal questions about ourselves and he has to answer them with yes or no.
+I am (abdalrahman alhmouz) the one who wrote this project in cooperation with my friends and teature tamim.
+we uesed ( HTML , JAVASCRIPT and css )
---
+++ js/app.js
@@ -0,0 +1,55 @@
+prompt("what is your name ?");
+var name = prompt("what is your name ?");
+alert(name);
+alert("hello"+name);
+
+var userInput = prompt("what is my name (abdalrahman or ahmad)?").toLowerCase(userInput);
+switch(userInput){
+case 'abdalrahman' :
+    alert("that true");
+break;
+case 'ahmad':
+alert('that false');
+break ;
+
+}
+var userInput2 = prompt("what is my hoppy (sport or drawing)?").toLowerCase(userInput2);
+switch(userInput2){
+case 'sport' :
+    alert("that true");
+break;
+case 'drawing':
+alert('that false');
+break ;
+
+}  
+var myTeam = prompt("Is my hair black?(yes or no )").toLowerCase(myTeam);
+if (myTeam === "yes") {
+    alert('true');}
+    // console.log("true");
+else {
+    alert('false');
+    //console.log("false");
+}
+console.log('myTeam');
+
+var myskin = prompt("Is my skin black?(yes or no )").toLowerCase(myskin);
+if (myskin === "yes") {
+    alert('false');}
+    // console.log("false");
+else {
+    alert('true');
+    //console.log("true");
+}
+console.log('myskin');
+
+var mylength = prompt("Is my length 174?(yes or no )").toLowerCase(mylength);
+if (mylength === "yes") {
+    alert('true');}
+    // console.log("true");
+else {
+    alert('false');
+    //console.log("false");
+}
+
+
